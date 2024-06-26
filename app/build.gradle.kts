ad08ea61c6d9dc1b03b26b36a3c37a4b29dc91d6
NgPhatbeepppp <yphat732@gmail.com>
2024-06-27 01:38:44 +0700

    Bản sửa 27/6:

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -7,8 +7,6 @@ android {
     namespace = "com.example.cnpm_lt_da_ta"
     compileSdk = 34
 
-
-
     defaultConfig {
         applicationId = "com.example.cnpm_lt_da_ta"
         minSdk = 27
@@ -32,28 +30,18 @@ android {
 }
 
 dependencies {
-    implementation("androidx.appcompat:appcompat:1.6.1")
-    implementation("com.google.android.material:material:1.9.0")  // 1.12.0 -> 1.9.0
-    implementation("com.google.firebase:firebase-auth:22.1.1")  // 23.0.0 -> 22.1.1
 
+    implementation("androidx.appcompat:appcompat:1.6.1")
+    implementation("com.google.android.material:material:1.11.0")
     implementation("androidx.constraintlayout:constraintlayout:2.1.4")
-    implementation("androidx.cardview:cardview:1.0.0")
-    implementation("androidx.viewpager2:viewpager2:1.0.0")      // 1.1.0 -> 1.0.0
-    implementation("androidx.recyclerview:recyclerview:1.2.1")   // 1.3.2 -> 1.2.1
+    implementation("com.google.firebase:firebase-auth:23.0.0")
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
 
-    implementation(platform("com.google.firebase:firebase-bom:32.2.0")) // 33.1.0 -> 32.2.0
+    implementation(platform("com.google.firebase:firebase-bom:33.1.0"))
     implementation("com.google.firebase:firebase-analytics")
     implementation("com.google.firebase:firebase-database")
-    implementation("com.google.firebase:firebase-storage")
 
-    //
-    implementation("de.hdodenhof:circleimageview:3.1.0")
-    //
-    implementation("com.github.bumptech.glide:glide:4.15.1")     // 4.16.0 -> 4.15.1
-
-    implementation("com.squareup.picasso:picasso:2.71828")
 }
 
--- app/src/main/java/com/example/cnpm_lt_da_ta/User/User.java
+++ app/src/main/java/com/example/cnpm_lt_da_ta/User/User.java
@@ -1,25 +1,18 @@
 package com.example.cnpm_lt_da_ta.User;
 
-import java.io.Serializable;
-
-public class User implements Serializable {
-    private String userId;
+public class User {
     private String email;
     private String role;
     private String name;
-    private String phone;
 
     public User() {
     }
 
-    public User(String userId, String email, String role, String name, String phone) {
-        this.userId = userId;
+    public User(String email, String role, String name) {
         this.email = email;
         this.role = role;
         this.name = name;
-        this.phone =phone;
     }
-    public  String getUserId() {return userId; }
 
     public String getEmail() {
         return email;
@@ -34,7 +27,6 @@ public class User implements Serializable {
     }
 
     // Setters
-    public  void setUserId(String userId) {this.userId = userId;}
     public void setEmail(String email) {
         this.email = email;
     }
